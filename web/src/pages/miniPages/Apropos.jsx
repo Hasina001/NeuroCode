7abbf4b9de0a59a5a66578334e112a911b68a84c
rodin <rodinwitman9@example.com>
2025-08-18 02:52:36 +0300

    A propos

--- web/src/pages/miniPages/Apropos.jsx
+++ web/src/pages/miniPages/Apropos.jsx
@@ -16,7 +16,9 @@ const Apropos = () => {
         >
           <div className="text-right">
             <h2 className=" text-2xl md:text-3xl lg:text-4xl font-medium leading-snug tracking-wide">
-              <span className="relative inline-block">
+              <span
+                className="relative inline-block"
+              >
                 L’essence de notre vision
                 <span className="absolute bottom-0 left-0 h-1 w-full bg-purple-500 
                 transform translate-y-1 scale-x-75 origin-left"></span>
@@ -78,76 +80,78 @@ const Apropos = () => {
           </motion.h1>
 
             {/* Presentation equipe */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12">
             
             {/* Bloc 01 */}
             <motion.div
-              className="bg-gradient-to-br from-purple-50 to-blue-50 rounded-xl p-8 
+              className="bg-gradient-to-br from-[#202B2D]/20 to-[#015967]/40 rounded-xl p-8 
               shadow-sm hover:shadow-md transition-shadow duration-300"
               initial={{ scale: 0.8, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.6 }}
             >
               <div className="text-purple-600 text-4xl mb-4">01</div>
-              <h3 className="text-xl font-medium mb-3 text-gray-800">Nom et Prenom</h3>
-              <p className="text-gray-800 font-light leading-relaxed">Expertise</p>
+              <h3 className="text-xl font-medium mb-3 text-gray-800">Sambatra Elia</h3>
+              <p className="text-gray-800 font-light leading-relaxed">Developpeur IA<br />  </p>
             </motion.div>
 
             {/* Bloc 02 */}
             <motion.div
-              className="bg-gradient-to-br from-blue-50 to-[#015967]/10 rounded-xl p-8 
+              className="bg-gradient-to-br from-[#202B2D]/20 to-[#015967]/40 rounded-xl p-8 
               shadow-sm hover:shadow-md transition-shadow duration-300"
               initial={{ scale: 0.8, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.6, delay: 0.2 }}
             >
               <div className="text-purple-600 text-4xl mb-4">02</div>
-              <h3 className="text-xl font-medium mb-3 text-gray-800">Nom et Prenom</h3>
-              <p className="text-gray-800 font-light leading-relaxed">Expertise</p>
+              <h3 className="text-xl font-medium mb-3 text-gray-800">Mr Aro</h3>
+              <p className="text-gray-800 font-light leading-relaxed">Developpeur Frontend</p>
             </motion.div>
 
             {/* Bloc 03 */}
             <motion.div
-              className="bg-gradient-to-br from-[#015967]/10 to-green-50 rounded-xl p-8 
+              className="bg-gradient-to-br from-[#202B2D]/20 to-[#015967]/40 rounded-xl p-8 
               shadow-sm hover:shadow-md transition-shadow duration-300"
               initial={{ scale: 0.8, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.6, delay: 0.4 }}
             >
               <div className="text-purple-600 text-4xl mb-4">03</div>
-              <h3 className="text-xl font-medium mb-3 text-gray-800">Nom et Prenom</h3>
-              <p className="text-gray-800 font-light leading-relaxed">Expertise</p>
+              <h3 className="text-xl font-medium mb-3 text-gray-800">RAJAONARIVELO Rodin Witman</h3>
+              <p className="text-gray-800 font-light leading-relaxed">Developpeur Fullstack</p>
             </motion.div>
 
             {/* Bloc 04 */}
             <motion.div
-              className="bg-gradient-to-br from-[#015967]/10 to-[#015967]/20 rounded-xl p-8 
+              className="bg-gradient-to-br from-[#202B2D]/20 to-[#015967]/40 rounded-xl p-8 
               shadow-sm hover:shadow-md transition-shadow duration-300"
               initial={{ scale: 0.8, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
-              transition={{ duration: 0.6, delay: 0.4 }}
+              transition={{ duration: 0.6, delay: 0.6 }}
             >
               <div className="text-purple-600 text-4xl mb-4">04</div>
-              <h3 className="text-xl font-medium mb-3 text-gray-800">Nom et Prenom</h3>
-              <p className="text-gray-800 font-light leading-relaxed">Expertise</p>
-            </motion.div>
-
-            {/* Bloc 05 */}
-            <motion.div
-              className="bg-gradient-to-br from-[#015967]/20 to-[#202B2D]/10 rounded-xl p-8 
-              shadow-sm hover:shadow-md transition-shadow duration-300"
-              initial={{ scale: 0.8, opacity: 0 }}
-              whileInView={{ scale: 1, opacity: 1 }}
-              transition={{ duration: 0.6, delay: 0.4 }}
-            >
-              <div className="text-purple-600 text-4xl mb-4">05</div>
-              <h3 className="text-xl font-medium mb-3 text-gray-800">Nom et Prenom</h3>
-              <p className="text-gray-800 font-light leading-relaxed">Expertise</p>
+              <h3 className="text-xl font-medium mb-3 text-gray-800">RAPARISON Ny Hasina</h3>
+              <p className="text-gray-800 font-light leading-relaxed">Developpeur Fullstack</p>
             </motion.div>
 
           </div>  
           
         </div>
+
+        <div className="mr-auto mb-12 lg:mb-20 pr-0 lg:pr-4 w-full lg:w-auto max-w-lg">
+          <motion.h1 
+            className="text-3xl md:text-4xl font-medium text-left mb-16"
+            initial={{ scale: 0.8, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.6 }}
+          >
+            <span className="relative inline-block">
+              Ce que nous avons accompli
+              <span className="absolute bottom-0 left-0 h-1 w-full bg-purple-500 
+                transform translate-y-1 scale-x-75 origin-left"></span>
+            </span>
+          </motion.h1>
+      </div>
         
       </div>
       
